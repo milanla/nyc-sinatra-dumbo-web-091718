3dfd90a8c46b77fab1e433f7e91d8f6fc538cefb
milanla <milan.lanlada@gmail.com>
2018-10-11 18:21:36 -0400

    added form

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,27 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    # display all the figures
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+
+  get '/figures/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
+    # binding.pry
+    erb :"/figures/new"
+  end
+
+  post '/figures' do
+    # binding.pry
+    @figure = Figure.create(name: params[:figure][:name])
+    @title = Title.create(name: params[:figure][:title])
+    @new_landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+
+    erb :"/figures/show"
+  end
+
+
 end
